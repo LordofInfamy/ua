c7d800a6ae7c223a28fc783f251b3f420334c6fb
LordofInfamy <rblxshadow516@hotmail.com>
2015-01-06 14:14:18 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,6 @@
 local sp = script.Parent
+local squid = workspace:FindFirstChild("Squiddy")
+
+for i = 0, i > 10
+    local nsq = squid:Clone(workspace)
+    local f = Instance.new("Fire", workspace)
