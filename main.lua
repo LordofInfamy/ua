8c3f6866096111d1f6522d3b3a75df9b8ed77dc9
LordofInfamy <rblxshadow516@hotmail.com>
2015-01-07 10:44:42 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,4 +3,6 @@ local squid = workspace:FindFirstChild("Squiddy")
 
 for i = 0, i > 10
     local nsq = squid:Clone(workspace)
-    local f = Instance.new("Fire", workspace)
+    local f = Instance.new("Fire", nsq)
+    nsq.Position = Vector3.new(math.random(0, 40), 40, math.random(0,40))
+    nsq.Velocity= Vector3.new(math.random(-100, 100), -100, math.random(-100, 100))
